bbb2fe8cd8115093dbc5a555ba2ac5989a848be5
ignaciowaak <binanceceypto9733@gmail.com>
2024-02-24 02:02:51 -0300

    update

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function mostrarOcultarMenu(){
 }
 
 function seleccionar(){
-    //oculto el menu una vez que selecciono una opcion
+    //ooculto el menu una vez que selecciono una opcion
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
